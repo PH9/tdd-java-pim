1cac1cb4bc070e25407e08c1dc474e0339ad6dd8
Wasith Theerapattrathamrong <phai@live.com>
2017-06-10 15:50:10 +0700

    Refactor Test ONE+1

--- src/test/java/CaptchaTest.java
+++ src/test/java/CaptchaTest.java
@@ -7,7 +7,6 @@ public class CaptchaTest {
     @Test
     public void captcha_OnePlus1() {
         Captcha captcha = new Captcha(1, 1, 1, 1);
-        String actual = captcha.getResult();
-        assertEquals("ONE+1", actual);
+        assertEquals("ONE+1", captcha.getResult());
     }
 }
